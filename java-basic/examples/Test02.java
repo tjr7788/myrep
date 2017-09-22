6b2d87728364ea3ee779afec15452f85221c82f4
tjr7788 <sig4213@naver.com>
2017-09-22 10:14:13 +0900

    '1'

--- java-basic/examples/Test02.java
+++
@@ -1,5 +0,0 @@
-class Test02{
-  public static void main(String[] args) {
-    System.out.println("안녕하세요!");
-  }
-}
--- java-basic/examples/Test03.java
+++
@@ -1,5 +0,0 @@
-class Test03{
-  public static void main(String[] args) {
-    System.out.println("안녕하세요!");
-  }
-}
